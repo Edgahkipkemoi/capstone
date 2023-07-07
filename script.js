04e74ccc1065697d436cee525606af55b31ab2c9
unknown <kipkemoiedgah@gmail.com>
2023-07-07 12:26:25 +0300

    Modified script.js

--- script.js
+++ script.js
@@ -36,26 +36,20 @@ const speakers = [
     occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
     description: 'Benkler studies scholars-based imrovement, and published his seminal book The Wealth in Creativity in 2006.',
   },
-  ];
-  
+];
 const appendFeatured = document.getElementById('featured-speakers');   
-  
-  const image = speakers.map((speak) => speak.image);
-  
-  const name = speakers.map((speak) => speak.name);
-  
-  const occupation = speakers.map((speak) => speak.occupation);
-  
-  const description = speakers.map((speak) => speak.description);
-  
-  appendFeatured.insertAdjacentHTML('afterbegin',
-    '<div class="fs-header"> <h2>Featured Speakers</h2> <hr class="hr-line"> </div>');
-  
-  for (let i = 0; i < speakers.length; i += 1) {
+const image = speakers.map((speak) => speak.image);
+const name = speakers.map((speak) => speak.name);
+const occupation = speakers.map((speak) => speak.occupation);
+const description = speakers.map((speak) => speak.description);
+appendFeatured.insertAdjacentHTML('afterbegin',
+  '<div class="fs-header"> <h2>Featured Speakers</h2> <hr class="hr-line"> </div>');
+
+for (let i = 0; i < speakers.length; i += 1) {
   appendFeatured.insertAdjacentHTML('beforeend', `
         <div class="fs-content"> 
             <div class="speaker">
-              <img src="${image[i]}" alt="Speaker 1" class="speaker-img">
+            <img src="${image[i]}" alt="Speaker 1" class="speaker-img">
               <div class="speaker-content">
                   <h2 class="speaker-title">${name[i]}</h2>
                   <p class="italic-text">
@@ -69,5 +63,4 @@ const appendFeatured = document.getElementById('featured-speakers');
             </div>
           </div>
       `);
-  }
-  
+}
